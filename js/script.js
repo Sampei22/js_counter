8c92d120219114b0cedcba110be293dce1d6b5ed
sampei22 <e.sanchini93@gmail.com>
2025-12-03 13:58:52 +0100

    Modificati i file: readme.md, index.html,style.css e script.js

--- js/script.js
+++ js/script.js
@@ -1,117 +1,106 @@
-const styleSheets = document.createElement("link")
-styleSheets.rel = "stylesheet"
-styleSheets.href = "css/style.css"
-document.head.appendChild(styleSheets)
-
-const favicon = document.createElement("link")
-favicon.rel = "icon"
-favicon.href = "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
-document.head.appendChild(favicon)
-
 const pointerBody = document.querySelector("body")
 
-const btnSfondo = document.createElement("button")
-btnSfondo.textContent = "Cambia lo sfondo"
-btnSfondo.setAttribute("class", "buttonStyle")
-btnSfondo.style.cssText="position:fixed; top:10px; left:10px; font-size:1rem;"
-pointerBody.appendChild(btnSfondo)
+const btnBackground = document.createElement("button")
+btnBackground.textContent = "Cambia lo sfondo"
+btnBackground.classList.add("buttonStyle", "buttonBackground")
+pointerBody.appendChild(btnBackground)
 
-const coloriSfondo = ["linear-gradient(to top, white,green)","linear-gradient(to top, white,blue)","linear-gradient(to top, white,red)","linear-gradient(to top, white,purple)","linear-gradient(to top, white,orange)"]
-let sfondoPrecedente = 0
+const backgroundColor = ["linear-gradient(to top, white,green)",
+                        "linear-gradient(to top, white,blue)",
+                        "linear-gradient(to top, white,red)",
+                        "linear-gradient(to top, white,purple)",
+                        "linear-gradient(to top, white,orange)"]
+let currentBackground = 0
 
-btnSfondo.addEventListener("click", ()=>{
-    let nuovoSfondo
+btnBackground.addEventListener("click", ()=>{
+    let newBackground
 
     do{
-        nuovoSfondo = Math.floor(Math.random() * coloriSfondo.length)
-    } while (nuovoSfondo === sfondoPrecedente);
+        newBackground = Math.floor(Math.random() * backgroundColor.length)
+    } while (newBackground === currentBackground);
     
-    pointerBody.style.background = coloriSfondo[nuovoSfondo]
-    sfondoPrecedente = nuovoSfondo
+    pointerBody.style.background = backgroundColor[newBackground]
+    currentBackground = newBackground
 })
 
 const divGrid = document.createElement("div")
 divGrid.setAttribute("class", "counterGrid")
 pointerBody.appendChild(divGrid)
 
-const titolo = document.createElement("h1")
-titolo.style.cssText = "grid-column:1/-1; text-align:center; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"
-titolo.textContent = "Contatore JavaScript"
-divGrid.appendChild(titolo)
+const title = document.createElement("h1")
+title.textContent = "Contatore JavaScript"
+divGrid.appendChild(title)
         
-const btnMeno = document.createElement("button")
-btnMeno.setAttribute("class","buttonStyle")
-btnMeno.textContent = "-"        
+const btnMinus = document.createElement("button")
+btnMinus.setAttribute("class","buttonStyle")
+btnMinus.textContent = "-"        
 
 const counter = document.createElement("span")
-counter.style.cssText="text-align:center; font-size:2rem; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"
 counter.textContent = 0
 
-const btnPiu = document.createElement("button")
-btnPiu.setAttribute("class","buttonStyle")
-btnPiu.textContent = "+"
+const btnPlus = document.createElement("button")
+btnPlus.setAttribute("class","buttonStyle")
+btnPlus.textContent = "+"
 
-const btnAzzera = document.createElement("button")
-btnAzzera.classList.add("buttonStyle","span3")
-btnAzzera.textContent = "Azzera contatore"
+const btnResetCounter = document.createElement("button")
+btnResetCounter.classList.add("buttonStyle","span3")
+btnResetCounter.textContent = "Azzera contatore"
 
-const btnSalva = document.createElement("button")
-btnSalva.classList.add("buttonStyle","span3")
-btnSalva.textContent = "Salva contatore"
+const btnSave = document.createElement("button")
+btnSave.classList.add("buttonStyle","span3")
+btnSave.textContent = "Salva contatore"
 
-const btnCanc = document.createElement("button")
-btnCanc.classList.add("buttonStyle","span3")
-btnCanc.textContent = "Cancella ultimo contatore salvato"
+const btnDeleteLast = document.createElement("button")
+btnDeleteLast.classList.add("buttonStyle","span3")
+btnDeleteLast.textContent = "Cancella ultimo contatore salvato"
 
-const btnSvuota = document.createElement("button")
-btnSvuota.classList.add("buttonStyle","span3")
-btnSvuota.textContent = "Svuota i contatori salvati"          
+const btnEmptyArray = document.createElement("button")
+btnEmptyArray.classList.add("buttonStyle","span3")
+btnEmptyArray.textContent = "Svuota i contatori salvati"          
 
-let arraySalvataggi = new Array()
-let i = 0 /*controllare*/
+let arraySave = []
+let numbSaves = 0
         
-divGrid.appendChild(btnMeno)
+divGrid.appendChild(btnMinus)
 divGrid.appendChild(counter)
-divGrid.appendChild(btnPiu)
-divGrid.appendChild(btnAzzera)
-divGrid.appendChild(btnSalva)
-divGrid.appendChild(btnCanc)
-divGrid.appendChild(btnSvuota)
+divGrid.appendChild(btnPlus)
+divGrid.appendChild(btnResetCounter)
+divGrid.appendChild(btnSave)
+divGrid.appendChild(btnDeleteLast)
+divGrid.appendChild(btnEmptyArray)
 
 const listaSalvataggi = document.createElement("ul")
 listaSalvataggi.classList.add("span3")
-listaSalvataggi.style.cssText="font-family:'Segoe UI',Tahoma,Geneva,Verdana,sans-serif; list-style-type:none; text-align:center;"
-        /* sistemare */
 listaSalvataggi.textContent="I tuoi contatori salvati:"
         
-btnMeno.addEventListener("click", ()=> {
+btnMinus.addEventListener("click", ()=> {
     counter.textContent = Number(counter.textContent) - 1;
 })
 
-btnPiu.addEventListener("click", ()=>{
+btnPlus.addEventListener("click", ()=>{
     counter.textContent = Number(counter.textContent) + 1;
 })
 
-btnAzzera.addEventListener("click", function(){
+btnResetCounter.addEventListener("click", ()=>{
     counter.textContent = "0"
 })
 
-btnSalva.addEventListener("click", function(){
-    i = salvaCont(counter.textContent, i, arraySalvataggi)      
+btnSave.addEventListener("click", ()=>{
+    numbSaves = salvaCont(counter.textContent, numbSaves, arraySave)      
 })
 
-btnCanc.addEventListener("click", function(){
-    if (i===0) return
-    if (i==1){
+btnDeleteLast.addEventListener("click", ()=>{
+    if (numbSaves===0) return
+    if (numbSaves==1){
         divGrid.removeChild(listaSalvataggi)
     }
-    arraySalvataggi.pop()
+    arraySave.pop()
     listaSalvataggi.removeChild(listaSalvataggi.lastChild)
-    i--
+    numbSaves--
 })
 
-btnSvuota.addEventListener("click", function(){
-    i = svuotaArray(arraySalvataggi)
+btnEmptyArray.addEventListener("click", ()=>{
+    numbSaves = svuotaArray(arraySave)
 })
 
 function salvaCont (num, indice, array){
